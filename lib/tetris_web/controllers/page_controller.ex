a6d907e8c832484c7a74d54b7494722ea7dc8825
Rogerio Bordignon <rog.bordignon@gmail.com>
2023-10-31 18:49:22 -0300

    redirect to /tetris url

--- lib/tetris_web/controllers/page_controller.ex
+++ lib/tetris_web/controllers/page_controller.ex
@@ -4,6 +4,6 @@ defmodule TetrisWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/tetris")
   end
 end
